222c16c8483612c291043b1555b451595608b074
MariaScoth <mariascoth@gmail.com>
2023-12-02 17:38:19 -0500

    Se suben los escenarios para las pruebas de navegación

---
+++ Cypress/cypress/e2e/modificar_path_secundarios.cy.js
@@ -0,0 +1,75 @@
+///<reference types="cypress"/>/// 
+import paginaInicioSesion from "../integration/PageObject/paginaInicioSesion"
+import mock_data from "../fixtures/MOCK_DATA.json"
+import '../support/commands'
+
+//import {Given,When, Then} from "cypress-cucumber-preprocessor"
+
+
+describe("Cypress POM Test Suite", function () {
+  const login = new paginaInicioSesion();
+  const title='';
+  const description='';
+
+  beforeEach('Realizar ingreso a Ghost', () => {
+      login.navegate();
+      login.enterEmail('j.scoth@uniandes.edu.co');
+      login.enterPassword('Maria9002$');
+      login.enviar();
+      // Ingresar a la sección Post
+      cy.get('#ember34').click();
+      
+      //Then location is Post
+      cy.url().then(url => {
+        if (url.includes('navigation')) {
+
+
+          // La URL actual contiene "posts"
+          console.log("La URL actual contiene navigation");
+        } else {
+          // La URL actual no contiene "posts"
+          console.log("La URL actual no contiene navigation");
+        }
+      })
+    
+
+  });
+  
+  it ('Escenario 1: Ingresar al boton Secundary y editar un Path con datos correctos ' , function () {
+
+  
+    // Cargar el archivo Json
+    cy.fixture('mock_data.json').then(data=> {
+      const title = data[0].title
+      const URL = ''
+      
+    
+    cy.wait(1000);
+    //Ingresar crear New POST
+    
+    cy.get('a').then(($a)=>{
+      cy.get(':nth-child(4) > :nth-child(2) > #navigation').first().click();
+      cy.screenshot();
+      cy.get('button:contains("Customize")').eq(1).click();
+      cy.screenshot();
+      cy.get('#secondary-nav').click();
+      cy.screenshot();
+      cy.get(':r86:').type(title);
+      cy.screenshot();
+      cy.get('#\:r11\:').type('/#/posts/');
+      cy.screenshot();
+      cy.get('.bg-black').click();
+      cy.screenshot();
+
+     // cy.get('button:contains("Secondary")').click();
+     
+    //Then I p have text description
+    cy.get('#\:r10\:').should('contain',title); 
+        
+    })
+   
+      
+    })
+  })
+
+  })
