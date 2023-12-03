8654459750d81e851aa59d378b84e924afc6acce
Juan Diego Lugo Sánchez <60227071+JuanLugoS@users.noreply.github.com>
2023-12-02 21:48:58 -0500

    backstop run

--- Cypress/Semana 8/backstop/data.py
+++ Cypress/Semana 8/backstop/data.py
@@ -3,15 +3,12 @@ data_t = {
     'vers2':2369,
     'nom_tests': 
         [
-            ('test1',10), 
-            ('test2',10), 
-            ('test3',10), 
-            ('test4',10), 
-            ('test5',10), 
-            ('test6',10), 
-            ('test7',10), 
-            ('test8',10),
-            ('test9',10), 
-            ('test10',10)
+            ('CancelarCrearPath',4), 
+            ('crearUnPath',3), 
+            ('eliminarRunPath',3), 
+            ('Home Page',3), 
+            ('ModificarURLinvalida',3), 
+            ('Screen Home Tag',4), 
+            ('Screen Settings',3)
         ]
     }
